b8d50582c203d5cf54d25c5bd537bce324d105ba
mroth3 <mcr040rcm@gmail.com>
2025-09-03 12:43:39 -0400

    Update paths for N_10000 dataset

--- models/multi_mma_univar_10000.R
+++ models/multi_mma_univar_10000.R
@@ -11,8 +11,8 @@ suppressPackageStartupMessages({
 # ----------------------------
 # 1) Paths & IO setup
 # ----------------------------
-base_dir <- "datasets/N_2000"
-out_dir  <- "estimates/N_2000"
+base_dir <- "datasets/N_10000"
+out_dir  <- "estimates/N_10000"
 plot_dir <- file.path(out_dir, "plots")
 
 dir.create(out_dir,  recursive = TRUE, showWarnings = FALSE)
